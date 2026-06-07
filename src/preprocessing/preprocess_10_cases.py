2a75684cfa61071cce5b9bd6e52a323ce6a3251c
NadiaAkram75 <105995924+NadiaAkram75@users.noreply.github.com>
2026-06-07 13:59:27 +0000

    resize data volumes to 128x128x128

--- src/preprocessing/preprocess_10_cases.py
+++ src/preprocessing/preprocess_10_cases.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import nibabel as nib
 import numpy as np
+from monai.transforms import Resize
+
 
 root = Path(
     "data/raw/brats/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData"
@@ -8,6 +10,8 @@ root = Path(
 
 output_dir = Path("data/processed/t1ce")
 output_dir.mkdir(parents=True, exist_ok=True)
+
+resize = Resize(spatial_size=(128, 128, 128))
 
 cases = sorted([p for p in root.iterdir() if p.is_dir()])[:10]
 
@@ -19,6 +23,9 @@ for case in cases:
     volume = nib.load(str(t1ce_file)).get_fdata()
 
     volume = (volume - volume.mean()) / volume.std()
+
+    volume = resize(volume[np.newaxis, ...])
+    volume = volume.squeeze(0)
 
     np.save(
         output_dir / f"{case_id}.npy",
